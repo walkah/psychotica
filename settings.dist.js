5e54b9ef3acdd41705317a9b4359ae65a98fa142
James Walker <walkah@walkah.net>
2011-03-12 18:14:03 -0500

    simple sample settings file

---
+++ settings.dist.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'site_name' : 'psychoti.ca', 
+  'port' : 3000, // http port to run on
+  'user' : '', // username to use for auth
+  'pass' : '', // password to use for auth
+  'secret' : '', // a super secret for salting hashes
+}
