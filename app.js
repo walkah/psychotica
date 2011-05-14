32bd8bd4a69747fa0bf4739bc4393b1850bc3f62
James Walker <walkah@walkah.net>
2011-05-14 13:57:59 -0400

    giving express-resource a whirl. thanks, tj!

--- app.js
+++ app.js
@@ -4,10 +4,10 @@
 var settings = require('./settings');
 var express = require('express');
 var app = module.exports = express.createServer();
+var resource = require('express-resource');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/psychotica_test');
 require('./models');
-
 var user = require('./user');
 
 // Configuration
@@ -39,41 +39,7 @@ app.dynamicHelpers({
 });
 
 // Routes
-app.get('/', function(req, res) {
-  var Activity = mongoose.model('Activity');
-  var query = Activity.find();
-  query.sort('created_on', -1);
-  query.limit(20);
-  query.exec(function(err, docs) {
-    res.render('index', {
-      title: settings.site_name,
-      activities: docs,
-    });
-  });
-});
-
-app.post('/post', function(req, res) {
-  var Activity = mongoose.model('Activity');
-  var a = new Activity();
-  a.object = req.body.object;
-  a.type = req.body.type;
-  a.save(function() {
-    res.redirect('/');
-  });
-});
-
-app.get('/activity/:id', function(req, res, next) {
-  var Activity = mongoose.model('Activity');
-  Activity.findOne({_id: req.param('id')}, function(err, doc) {
-    if (err || !doc) {
-      return next(new Error('Activity not found'));
-    }
-    res.render('activity', {
-      title: settings.site_name,
-      activity: doc,
-    });
-  });
-});
+app.resource('activity', require('./lib/activity'));
 
 // User stuff
 app.get('/login', user.login);
@@ -85,5 +51,5 @@ app.post('/settings', user.logged_in, user.save);
 // Start the server
 if (!module.parent) {
   app.listen(settings.port);
-  console.log("psychotica listening on port %d", app.address().port)
+  console.log("psychotica listening on port %d", app.address().port);
 }
