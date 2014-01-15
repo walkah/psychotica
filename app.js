bfe2297731e8e51b54e895fb9fd8bf92f10debc7
James Walker <walkah@walkah.net>
2014-01-14 22:42:58 -0500

    rebooting as a fresh express app

--- app.js
+++ app.js
@@ -1,79 +1,39 @@
+
 /**
- * Psychotica
+ * Module dependencies.
  */
-var settings = require('./settings');
+
 var express = require('express');
-var app = module.exports = express.createServer();
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
 
-var MongoStore = require('connect-mongodb');
-var mongoose = require('mongoose'); 
-mongoose.connect(settings.db_url);
+var app = express();
 
-require('express-resource');
-var passport = require('passport')
-  , BrowserIDStrategy = require('passport-browserid').Strategy;
-
-passport.serializeUser(function(user, done) {
-  done(null, user.email);
-});
-
-passport.deserializeUser(function(email, done) {
-  done(null, { email: email });
-});
-
-passport.use(new BrowserIDStrategy({
-    audience: 'http://0.0.0.0:3000'
-  },
-  function(email, done) {
-    console.log(email);
-    if (email == settings.email) {
-      return done(null, {email: email});
-    }
-  }
-));
-
-// Config
-app.set('views', __dirname + '/views');
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Middleware
-app.configure(function(){
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: settings.secret, 
-                            store: new MongoStore({db: mongoose.connections[0].db}) }));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-// Routes
-app.get('/login', function(req, res){
-  res.render('login', { title: settings.site_name, user: req.user });
-});
-
-app.post(
-  '/auth/browserid', 
-  passport.authenticate('browserid', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
-
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
-});
-
-app.resource(require('./routes/site'));
-app.resource('activity', require('./routes/activity'));
-
-
-// Start the server
-if (!module.parent) {
-  app.listen(settings.port);
-  console.log("psychotica running at http://%s:%d", app.address().address, app.address().port);
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
 }
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
--- settings.dist.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'site_name' : 'psychoti.ca', 
-  'db_url' : 'mongodb://localhost/psychotica',
-  'port' : 3000, // http port to run on
-  'secret' : '', // a super secret for salting hashes
-  'email' : '' // email address for site user
-}
